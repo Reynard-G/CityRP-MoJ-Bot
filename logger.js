2681fcbadf7d6491925f9783d0ab0dcaea171c71
Reynard-G <reynard.ean.gunawan@gmail.com>
2024-06-23 15:15:04 -0500

    chore: Add winston logging library for improved logging functionality

--- logger.js
+++ logger.js
@@ -1,12 +1,47 @@
+const winston = require("winston");
 const chalk = require("chalk");
 
 const pipe = chalk.dim(chalk.gray("|"));
 const left = chalk.dim(chalk.gray("["));
 const right = chalk.dim(chalk.gray("]"));
 
-function getDate() {
-  const date = new Date();
+const levels = {
+  error: 0,
+  warn: 1,
+  info: 2,
+  success: 3,
+  debug: 4,
+};
 
+const colors = {
+  error: "red",
+  warn: "yellow",
+  info: "white",
+  success: "green",
+  debug: "cyan",
+};
+
+const icons = {
+  error: "!",
+  warn: "*",
+  info: "?",
+  success: "$",
+  debug: "i",
+};
+
+winston.addColors(colors);
+
+// Function to strip ANSI color codes
+function stripAnsi(str) {
+  const pattern = [
+    "[\\u001B\\u009B][[\\]()#;?]*(?:(?:(?:(?:;[-a-zA-Z\\d\\/#&.:=?%@~_]+)*|[a-zA-Z\\d]+(?:;[-a-zA-Z\\d\\/#&.:=?%@~_]*)*)?\\u0007)",
+    "(?:(?:\\d{1,4}(?:;\\d{0,4})*)?[\\dA-PR-TZcf-nq-uy=><~]))",
+  ].join("|");
+  return str.replace(new RegExp(pattern, "g"), "");
+}
+
+const getTimestamp = () => {
+  const date = new Date();
   const options = {
     year: "numeric",
     month: "2-digit",
@@ -17,53 +52,59 @@ function getDate() {
     fractionalSecondDigits: 3,
     hour12: false,
   };
-
   return new Intl.DateTimeFormat("en-US", options)
     .format(date)
     .replace(",", "");
-}
-
-function log(level, message, path) {
-  const levels = {
-    info: {
-      icon: "?",
-      color: chalk.white,
-    },
-    success: {
-      icon: "$",
-      color: chalk.green,
-    },
-    warning: {
-      icon: "*",
-      color: chalk.yellow,
-    },
-    error: {
-      icon: "!",
-      color: chalk.red,
-    },
-    debug: {
-      icon: "i",
-      color: chalk.cyan,
-    },
-  };
-
-  const levelInfo = levels[level] || levels.info;
-
-  const formattedDate = chalk.dim(chalk.white(`${left}${getDate()}${right}`));
-
-  console.log(
-    levelInfo.color(
-      `${formattedDate} ${left}${levelInfo.icon}${right} ${pipe} ${chalk.bold(
-        message
-      )}${path ? ` ${pipe} ${path}` : ""}`
-    )
-  );
-}
-
-module.exports = {
-  info: (message, path) => log("info", message, path),
-  success: (message, path) => log("success", message, path),
-  warning: (message, path) => log("warning", message, path),
-  error: (message, path) => log("error", message, path),
-  debug: (message, path) => log("debug", message, path),
 };
+
+const consoleFormat = winston.format.printf(
+  ({ level, message, timestamp, path }) => {
+    const color = colors[level];
+    const icon = icons[level];
+    const formattedDate = chalk.dim(chalk.white(`${left}${timestamp}${right}`));
+
+    return chalk[color](
+      `${formattedDate} ${left}${icon}${right} ${pipe} ${chalk.bold(message)}${path ? ` ${pipe} ${path}` : ""}`
+    );
+  }
+);
+
+const fileFormat = winston.format.printf(
+  ({ level, message, timestamp, path }) => {
+    const icon = icons[level];
+    return `[${timestamp}] [${icon}] | ${stripAnsi(message)}${path ? ` | ${path}` : ""}`;
+  }
+);
+
+const logger = winston.createLogger({
+  levels: levels,
+  level: "debug",
+  transports: [
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.errors({ stack: true }),
+        winston.format.timestamp({ format: getTimestamp }),
+        consoleFormat
+      ),
+    }),
+    new winston.transports.File({
+      filename: "logs/combined.log",
+      format: winston.format.combine(
+        winston.format.errors({ stack: true }),
+        winston.format.timestamp({ format: getTimestamp }),
+        fileFormat
+      ),
+    }),
+    new winston.transports.File({
+      filename: "logs/error.log",
+      level: "error",
+      format: winston.format.combine(
+        winston.format.errors({ stack: true }),
+        winston.format.timestamp({ format: getTimestamp }),
+        fileFormat
+      ),
+    }),
+  ],
+});
+
+module.exports = logger;
